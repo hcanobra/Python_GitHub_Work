253187b4c0f8cb706c1781d336a17c8cac63cc89
hcanobra <hcanobra@gmail.com>
2023-05-30 09:07:46 -0600

    updates on prometheus push

--- Python_prometheus/python_prometheus_push.py
+++ Python_prometheus/python_prometheus_push.py
@@ -1,11 +1,14 @@
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+import time
 
- 
+for i in range (5):
 
-registry = CollectorRegistry()
-
-g = Gauge('job_last_success_unixtime', 'Last time a batch job successfully finished', registry=registry)
-
-g.set_to_current_time()
-
-push_to_gateway('15.181.163.0:9091', job='MEC_Dreamscape', registry=registry)
+    registry = CollectorRegistry()    
+    g = Gauge('job_last_success_unixtime', 'Another metric', registry=registry)
+    g.set_to_current_time()
+    
+    push_to_gateway('15.181.163.0:9091', 
+                    job='MEC_Dreamscape', 
+                    registry=registry)
+    print ('Done... ')
+    time.sleep (3)
